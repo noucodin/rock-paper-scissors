dbbfa1a8c4a861e79018730cf21b6a7d1d894329
nour <nour06p@gmail.com>
2022-10-18 11:47:49 +0200

    Finished the game logic

--- script.js
+++ script.js
@@ -5,3 +5,41 @@ const buttons = document.querySelectorAll(".Btn");
 let player;
 let computer;
 let result;
+
+buttons.forEach((button) =>
+  button.addEventListener("click", () => {
+    player = button.textContent;
+    computerTurn();
+    playerText.textContent = `Player: ${player}`;
+    computerText.textContent = `Computer: ${computer}`;
+    resultText.textContent = checkWinner();
+  })
+);
+
+function computerTurn() {
+  const randomNumber = Math.floor(Math.random() * 3) + 1;
+
+  switch (randomNumber) {
+    case 1:
+      computer = "ROCK";
+      break;
+    case 2:
+      computer = "PAPER";
+      break;
+    case 3:
+      computer = "SCISSORS";
+      break;
+  }
+}
+
+function checkWinner() {
+  if (player == computer) {
+    return "It's a Draw!";
+  } else if (computer == "ROCK") {
+    return player == "PAPER" ? "You Win!" : "You Lose!";
+  } else if (computer == "PAPER") {
+    return player == "SCISSORS" ? "You Win!" : "You Lose!";
+  } else if (computer == "SCISSORS") {
+    return player == "ROCK" ? "You Win!" : "You Lose!";
+  }
+}
